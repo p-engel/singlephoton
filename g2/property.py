ff733e5af1841b94c036448034edad7a0c3166d9
p-engel <anyanc>
2023-07-07 15:22:58 -0700

    code to compute second order correlation using maseter equation solver on QuTip

---
+++ g2/property.py
@@ -0,0 +1,92 @@
+"""A set of classes for calculating the properties of the system"""
+import numpy as np
+
+# from qutip.rhs_generate import rhs_clear
+# from qutip.cy.utilities import _cython_build_cleanup
+# from qutip.states import ket2dm
+from qutip.qobj import Qobj #, isket
+from qutip.correlation import correlation_3op_1t
+from qutip.correlation import correlation_3op_2t
+from qutip.correlation import _correlation_me_2t
+from qutip.mesolve import mesolve
+from qutip.solver import Options, Result
+
+from system import MasterEquation, InitialState
+
+
+# class SecondOrderCorrelation:
+class Properties:
+	""" Define parameters, system, and state"""
+	def __init__(self, w, t, tau, na=1):
+		self.w = w								# drive frequency [eV]
+		self.t = t								# time [s]
+		self.tau = tau							# delay times >= 0 [s]
+		self.na = na							# initial cavity fock-state number
+		self.lindblad = MasterEquation(self.w)
+		self.state = InitialState(self.na)
+
+	def denomg2(self):
+		"""
+		Compute the normalization factor for
+		the non-steadystate second-order correlation function of
+		the light transmitted through the discrete mode
+		i.e normalize with n(t)*n(t+tau) to obtain g2
+		"""
+		H, c_ops, a, _ = self.lindblad.operators()
+		rho0 = self.state.rho()
+		norm_mat = np.zeros([np.size(self.t), np.size(self.tau)], dtype=complex)
+		# first calculate the occupation number as a function of initial time
+		n_t1 = mesolve(H, rho0, self.t, c_ops, [a.dag()*a]).expect[0]
+
+		for t_idx, tdelay in enumerate(self.tau):
+			# next calculate the occupation number as a function of delay time
+			n_t2 = mesolve(H, rho0, (self.t + tdelay), c_ops, [a.dag()*a]).expect[0]
+			norm_mat[:,t_idx] = n_t1 * n_t2
+
+		# np.outer(n_t, n_tau, out=norm_mat)
+
+		return norm_mat
+		
+	def computeg2(self):
+		"""
+		Compute the normalized second-order correlation 
+		of light transmitted through the discrete mode
+		"""
+		H, c_ops, a, _ = self.lindblad.operators()
+		rho0 = self.state.rho()
+		# rho0 = None
+		G2 = correlation_3op_2t(H, rho0, self.t, self.tau,
+			c_ops, a.dag(), a.dag()*a, a, solver="me", 
+			# options=Options(ntraj=[20, 100]))
+			)
+		norm_const = self.denomg2()
+		g2 = G2 / norm_const
+
+		return g2, G2, norm_const
+
+	def computeg2_equaltime(self, options=Options(ntraj=[20, 100])):
+	    """
+	    Hacking qutip's internal function "_correlation_me_2t()" for 
+	    calculating the three-operator, two-time,
+	    correlation function at tau=0:
+	    <A(t)B(t)C(t)>
+	    using a master equation solver.
+	    The solver only works for positive time differences and 
+	    the correlator require positive tau
+	    """
+	    H, c_ops, a, _ = self.lindblad.operators()
+	    rho0 = self.state.rho()
+	    a_op = a.dag()
+	    b_op = a.dag()*a
+	    c_op = a
+
+	    n = mesolve(H, rho0, self.t, c_ops, [a_op*c_op]).expect[0]
+	    G2_of_zero = _correlation_me_2t(H, rho0, self.t, [0], 
+			c_ops, a_op, b_op, c_op)
+	    g2_of_zero = G2_of_zero[:,0] / (n*n)
+
+	    return g2_of_zero, G2_of_zero[:,0], n*n
+
+
+## eighty five characters in a line
+#123456789012345678901234567890123456789012345678901234567890123456789012345678901234
---
+++ g2/system.py
@@ -0,0 +1,61 @@
+from math import sqrt
+from qutip.qobj import Qobj
+from qutip.tensor import tensor
+from qutip.operators import destroy, qeye
+from qutip.states import basis
+
+
+class MasterEquation:
+	"""
+	Defines the hamiltonian and collapse operators of 
+	two coupled bosons
+	"""
+	#constants
+	def __init__(self, w):
+		"""
+		known attributes of the parameters of the system in eV
+		"""
+		# self.hbar = 6.582119569e-16				# [ev*s]
+		self.hbar = 1								# unitless; t has units [1/eV]
+		self.we = 0.9326 / self.hbar  				#resonant freq. [rad/s]
+		self.wc = self.we - (20e-3) / self.hbar
+		self.game = (67.5e-3) / self.hbar			# dissipative rate [rad/s]
+		self.gamc = 6.075e-10 * self.game
+		self.g = 0.332e-3 / self.hbar				# coupling constant [rad/s]
+		self.v = 0.002355 * 2*self.game					# drive amplitude [eV/hbar]
+		self.w = w / self.hbar						# drive frequency [rad/s]
+		self.N = 9					 				# levels in Hilbert space
+
+	def operators(self):
+		""" 
+		define the hamiltonian and collapse operators of 
+		two coupled bosons
+		"""
+		a = tensor(destroy(self.N), qeye(self.N))
+		b = tensor(qeye(self.N), destroy(self.N))
+		H = self.hbar * (
+			(self.wc - self.w)*a.dag()*a
+			+ (self.we - self.w)*b.dag()*b
+			+ self.g*(a.dag()*b + b.dag()*a)
+			+ self.v*(b + b.dag())
+			)
+		Cops = [sqrt(2*self.gamc)*a, sqrt(2*self.game)*b]
+
+		return H, Cops, a, b
+
+
+class InitialState:
+	"""The initial state of cavity and emitter"""
+	def __init__(self, na):
+		self.na = na		# initial cavity fock-state number
+		self.N = 9			# levels in Hilbert space
+
+	def rho(self):
+		"""define initial state of cavity and emitter"""
+		rho0 = tensor(basis(self.N, self.na), basis(self.N, 0))
+
+		return rho0
+
+
+## eighty five characters in a line
+#123456789012345678901234567890123456789012345678901234567890123456789012345678901234
