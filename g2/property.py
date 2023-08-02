9cd85788240c393e56b1353cc2335e974cca5b3e
p-engel <anyanc>
2023-08-01 20:19:13 -0700

    create parameter object

--- g2/property.py
+++ g2/property.py
@@ -1,9 +1,6 @@
 """A set of classes for calculating the properties of the system"""
 import numpy as np
 
-# from qutip.rhs_generate import rhs_clear
-# from qutip.cy.utilities import _cython_build_cleanup
-# from qutip.states import ket2dm
 from qutip.qobj import Qobj #, isket
 from qutip.correlation import correlation_3op_1t
 from qutip.correlation import correlation_3op_2t
@@ -11,59 +8,18 @@ from qutip.correlation import _correlation_me_2t
 from qutip.mesolve import mesolve
 from qutip.solver import Options, Result
 
-from system import MasterEquation, InitialState
+from system import Parameters MasterEquation
 
 
 # class SecondOrderCorrelation:
 class Properties:
 	""" Define parameters, system, and state"""
-	def __init__(self, w, t, tau, na=0, nb=0):
+	def __init__(self, w, na=0, nb=0):
 		self.w = w								# drive frequency [eV]
-		self.t = t								# time [s]
-		self.tau = tau							# delay times >= 0 [s]
 		self.na = na							# initial cavity fock-state number
 		self.nb = nb							# initial emitter fock-state number
+		self.pars = Parameters(self.w)
 		self.lindblad = MasterEquation(self.w)
-		self.state = InitialState(self.na, self.nb)
-
-	def denomg2(self):
-		"""
-		Compute the normalization factor for
-		the non-steadystate second-order correlation function of
-		the light transmitted through the discrete mode
-		i.e normalize with n(t)*n(t+tau) to obtain g2
-		"""
-		H, c_ops, a, _ = self.lindblad.operators()
-		rho0 = self.state.rho()
-		norm_mat = np.zeros([np.size(self.t), np.size(self.tau)], dtype=complex)
-		# first calculate the occupation number as a function of initial time
-		n_t1 = mesolve(H, rho0, self.t, c_ops, [a.dag()*a]).expect[0]
-
-		for t_idx, tdelay in enumerate(self.tau):
-			# next calculate the occupation number as a function of delay time
-			n_t2 = mesolve(H, rho0, (self.t + tdelay), c_ops, [a.dag()*a]).expect[0]
-			norm_mat[:,t_idx] = n_t1 * n_t2
-
-		# np.outer(n_t, n_tau, out=norm_mat)
-
-		return norm_mat
-		
-	def computeg2(self):
-		"""
-		Compute the normalized second-order correlation 
-		of light transmitted through the discrete mode
-		"""
-		H, c_ops, a, _ = self.lindblad.operators()
-		rho0 = self.state.rho()
-		# rho0 = None
-		G2 = correlation_3op_2t(H, rho0, self.t, self.tau,
-			c_ops, a.dag(), a.dag()*a, a, solver="me", 
-			# options=Options(ntraj=[20, 100]))
-			)
-		norm_const = self.denomg2()
-		g2 = G2 / norm_const
-
-		return g2, G2, norm_const
 
 	def computeg2_equaltime(self, options=Options(ntraj=[20, 100])):
 	    """
@@ -75,20 +31,26 @@ class Properties:
 	    The solver only works for positive time differences and 
 	    the correlator require positive tau
 	    """
-	    H, c_ops, a, b = self.lindblad.operators()
-	    rho0 = self.state.rho()
+	    times, _ = self.pars.times()
+	    taus = [0]
+	    H, c_ops, a, b, rho0 = self.lindblad.operators()
+	    ops = [a, b]
+	    g2list = []
+	    nlist = []
 
-	    na = mesolve(H, rho0, self.t, c_ops, [a.dag()*a]).expect[0]
-	    nb = mesolve(H, rho0, self.t, c_ops, [b.dag()*b]).expect[0]
+	    for it_op, op in enumerate(ops):
+	    	# average occupation number
+	    	n = mesolve(H, rho0, times, c_ops, [op.dag()*op]).expect[0]
+	    	# second order correlation
+	    	G2 = _correlation_me_2t(H, rho0, times, taus, 
+				c_ops, op.dag(), op.dag()*op, op)
+	    	# normalized second order correlation
+	    	g2 = G2[:,0] / (n**2)
+	    	# add to list
+	    	nlist.append()
+	    	g2list.append()
 
-	    G2_tau0_a = _correlation_me_2t(H, rho0, self.t, [0], 
-			c_ops, a.dag(), a.dag()*a, a)
-	    g2_tau0_a = G2_tau0_a[:,0] / (na*na)
-	    G2_tau0_b = _correlation_me_2t(H, rho0, self.t, [0], 
-			c_ops, b.dag(), b.dag()*b, b)
-	    g2_tau0_b = G2_tau0_b[:,0] / (nb*nb)
-
-	    return g2_tau0_a, g2_tau0_b, na, nb
+	    return g2list, nlist
 
 
 ## eighty five characters in a line
--- g2/system.py
+++ g2/system.py
@@ -1,11 +1,65 @@
-from math import sqrt
+from cmath import pi, sqrt
+from numpy import linspace
+
 from qutip.qobj import Qobj
 from qutip.tensor import tensor
 from qutip.operators import destroy, qeye
 from qutip.states import basis
 
 
-class MasterEquation:
+class Parameters():
+	"""define the parameters for two coupled oscillators"""
+	def __init__(self, w):
+		"""
+		the parameters of the system in eV, based on 
+		the measured Fano system published in Nano Lett. by the Masiello group
+		"""
+		# self.hbar = 6.582119569e-16				# [ev*s]
+		self.hbar = 1								# unitless; t has units [1/eV]
+		self.we = 0.9326 / self.hbar  				# resonant freq. [rad/s]
+		self.wc = self.we - (20e-3) / self.hbar
+		self.game = (67.5e-3) / self.hbar			# dissipative rate [rad/s]
+		self.gamc = 6.075e-10 * self.game
+		self.g = 0.332e-3 / self.hbar				# coupling constant [rad/s]
+		self.v = 1*0.002355 * 2*self.game			# drive amplitude [eV/hbar]
+		self.w = w / self.hbar			 				# levels in Hilbert space
+
+	def resonances(self):
+		"""define important resonant frequencies across the Fano profile"""
+		# Fano parameters
+		eta_e = g**2 / ((self.w - we)**2 + (game/2)**2)
+		Omegac = wc + eta_e*(self.w - we)
+		Gamc = gamc + eta_e*game
+		q = (Omegac - (wc - 1j*gamc/2)) / (Gamc/2)
+		# solutions to the first derivative of fano function equal to zero
+		term = (1 - abs(q)**2) / (2*q.real)
+		epsq_plus = term + sqrt(term**2 + 1)
+		epsq_minus = term - sqrt(term**2 + 1)
+		# important resonant frequencies across Fano profile
+		# for negative q
+		wpeak = epsq_minus*(Gamc/2) + Omegac
+		wdip = epsq_plus*(Gamc/2) + Omegac
+		wres = Omegac
+		wlist = [wpeak, wres, wdip]
+
+		return wlist
+
+	def times(self):
+		"""Define time domain and scale"""
+		npts = 300
+		T_we = 2*pi / we					# period [rad/eV]
+		T_wc = 2*pi / wc
+		tau_wc = 2*pi / gamc				# coherence time
+		tau_we = 2*pi / game
+		tstep = period_we
+		endpt = npts * tstep
+		t = linspace(0, endpt, npts)
+		tau = t
+
+		return t, tau
+
+
+class MasterEquation(Parameters):
 	"""
 	Defines the hamiltonian and collapse operators of 
 	two coupled bosons
@@ -13,18 +67,12 @@ class MasterEquation:
 	#constants
 	def __init__(self, w):
 		"""
-		known attributes of the parameters of the system in eV
+		known attributes of the parameters of the system
 		"""
-		# self.hbar = 6.582119569e-16				# [ev*s]
-		self.hbar = 1								# unitless; t has units [1/eV]
-		self.we = 0.9326 / self.hbar  				#resonant freq. [rad/s]
-		self.wc = self.we - (20e-3) / self.hbar
-		self.game = (67.5e-3) / self.hbar			# dissipative rate [rad/s]
-		self.gamc = 6.075e-10 * self.game
-		self.g = 0.332e-3 / self.hbar				# coupling constant [rad/s]
-		self.v = 1*0.002355 * 2*self.game					# drive amplitude [eV/hbar]
-		self.w = w / self.hbar						# drive frequency [rad/s]
-		self.N = 9					 				# levels in Hilbert space
+		super.__init__(w)
+		self.N = 9					# levels in Hilbert space
+		self.na = na				# initial cavity fock-state number
+		self.nb = nb				# initial emitter fock-state number
 
 	def operators(self):
 		""" 
@@ -43,14 +91,6 @@ class MasterEquation:
 
 		return H, Cops, a, b
 
-
-class InitialState:
-	"""The initial state of cavity and emitter"""
-	def __init__(self, na, nb):
-		self.na = na		# initial cavity fock-state number
-		self.nb = nb		# initial emitter fock-state number
-		self.N = 9			# levels in Hilbert space
-
 	def rho(self):
 		"""define initial state of cavity and emitter"""
 		rho0 = tensor(basis(self.N, self.na), basis(self.N, self.nb))
@@ -58,5 +98,6 @@ class InitialState:
 		return rho0
 
 
-## eighty five characters in a line
-#123456789012345678901234567890123456789012345678901234567890123456789012345678901234
+
+## seventy-nine characters in a line
+#123456789012345678901234567890123456789012345678901234567890123456789012345678
